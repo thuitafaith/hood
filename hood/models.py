0bd8a54b94a6fa823ec6ae02290f5629594fe86c
thuitafaith <thuitamuthoni15@gmail.com>
2018-05-25 15:24:16 +0300

    create new file tokens.py

--- hood/models.py
+++ hood/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 # Create your models here.
 """
 Initialiazing the Profile model
@@ -11,6 +14,7 @@ class Profile(models.Model):
     bio = models.TextField(blank=True,null=True)
     location= models.CharField(max_length=100,null=True)
     email= models.CharField(max_length=100)
+    email_confirmed = models.BooleanField(default=False)
     national_identity = models.CharField(max_length=50)
     neighborhood = models.ForeignKey('Neighborhood', related_name ='hood_area',null=True)
 
@@ -18,7 +22,12 @@ class Profile(models.Model):
         self.save()
     def delete_profile(self):
         self.delete()
-    
+@receiver(post_save, sender=User)
+def update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user_profile=instance)
+    instance.profile.save()
+
 
 """
 Initialiazing the Neighborhood model
